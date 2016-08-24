0dd1dfc73fe9c7852dee29f18f26b51cc2c08f6a
caiomeriguetti <caiomeriguetti@gmail.com>
2016-08-24 10:09:17 -0300

    dados do elastic numa pasta compartilhada do container

--- docker-container/run-container.sh
+++ docker-container/run-container.sh
@@ -2,4 +2,4 @@
 
 sudo docker stop appmonitor
 sudo docker rm appmonitor
-sudo docker run -t -d -p 9201:9200 -p 6868:10000/udp -p 5601:5601 --name appmonitor monit:appmonitor /bin/bash
+sudo docker run -t -d -v /var/elasticdata:/var/elasticdata -p 9201:9200 -p 6868:10000/udp -p 5601:5601 --name appmonitor monit:appmonitor /bin/bash
