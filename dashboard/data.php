d8d2d958547cc0bcd082f028635c99ad14e8e540
caiomeriguetti <caiomeriguetti@gmail.com>
2016-08-25 17:43:59 -0300

    getting last events

--- dashboard/data.php
+++ dashboard/data.php
@@ -1,5 +1,35 @@
 <?php
 
-$searchData = file_get_contents('http://localhost:9200/time-spent/_search?q=*&pretty');
 
-echo $searchData;
+$currentTime = time();
+$minutesToMonitor = 1;
+$delta = $minutesToMonitor*60;
+
+$data = array('query' => array(
+        'range' => array(
+                'timestamp' => array(
+                        'gte' => $currentTime - $delta,
+                        'lte' => $currentTime
+                )
+        )
+));
+
+$url = 'http://localhost:9200/time-spent/_search';
+
+//open connection
+
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, 1);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($data));
+
+//execute post
+$result = curl_exec($ch);
+
+//close connection
+curl_close($ch);
+
+echo $result;
