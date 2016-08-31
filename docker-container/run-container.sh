1b93af92f7dccef438d418cf254dff88657774cb
caiomeriguetti <caiomeriguetti@gmail.com>
2016-08-31 17:04:51 -0300

    adjustments in apache config

--- docker-container/run-container.sh
+++ docker-container/run-container.sh
@@ -2,4 +2,4 @@
 
 sudo docker stop appmonitor
 sudo docker rm appmonitor
-sudo docker run -t -d -v /var/app-monitor-data:/var/app-monitor-data -p 9201:9200 -p 81:80 -p 6868:10000/udp -p 5601:5601 --name appmonitor monit:appmonitor /bin/bash
+sudo docker run -t -d -v /var/app-monitor-data:/var/app-monitor-data -p 9201:9200 -p 81:81 -p 6868:10000/udp -p 5601:5601 --name appmonitor monit:appmonitor /bin/bash
